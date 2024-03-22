acf2e3e49dd806fc76c4718b23a4d97dbe54d2b6
Natalia Horbunova <nata.horbunova@gmail.com>
2024-03-22 03:34:16 +0200

    homepage layout

--- app.js
+++ app.js
@@ -8,8 +8,17 @@ const{ port } = server;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
-app.set('views', 'views'); 
-app.use('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=> {
+    res.render('index');
+});
+
+app.get('/user-posts', (req, res)=> {
+    res.render('user_posts');
+});
 
 
 app.listen(port, console.log(`Server is running on port ${port}`));
